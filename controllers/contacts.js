9963047f8416716a4d9701014c036d76bd731e82
benwasden <was24001@byui.edu>
2025-09-11 18:09:34 -0700

    Week 2 start

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -18,4 +18,47 @@ const getSingle = async (req, res) => {
     });
 };
 
-module.exports = { getAll, getSingle };
+const createContact = async (req, res) => {
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('contacts').insertOne(user);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while adding the contact.');
+    };
+};
+
+const updateContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: userId }, user);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while updating the contact.');
+    };
+};
+
+const deleteContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: userId });
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while deleting the contact');
+    };
+}
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongodb = require('./data/database.js');
+const bodyParser = require('body-parser');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
 
 
