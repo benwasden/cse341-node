9039a5cf13cd0e9a4661f8d068359e59fcdb8cb3
benwasden <was24001@byui.edu>
2025-09-17 15:36:42 -0700

    Week 3 Team Start

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,12 @@ const ObjectId = require('mongodb').ObjectId;
 const getAll = async (req, res) => {
     //#swagger.tags=['Contacts']
     const result = await mongodb.getDatabase().db().collection('contacts').find();
-    result.toArray().then((contacts) => {
+    result.toArray((err) => {
+        if (err) {
+            res.status(400).json({ message: err });
+        }
+    }
+    ).then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts);
     });
@@ -14,7 +19,11 @@ const getSingle = async (req, res) => {
     //#swagger.tags=['Contacts']
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: userId });
-    result.toArray().then((contacts) => {
+    result.toArray((err) => {
+        if (err) {
+            res.status(400).json({ message: err });
+        }
+    }).then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
     });
